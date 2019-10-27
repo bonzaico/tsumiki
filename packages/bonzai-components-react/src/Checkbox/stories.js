38a24a4981d6abb64000626fb41dd2f877e6fa32
Debjit Biswas <debjit@fastmail.fm>
2019-10-27 22:50:53 +0530

    feat(react): Add typescript support and add button
    For: ENG-29
    
    Change-Id: I847181ca2602f904356a22ff07bac932988fdd1f

--- packages/bonzai-components-react/src/Checkbox/stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Checkbox } from "../Checkbox";
-
-storiesOf("Checkbox")
-    .add("without props", () => <Checkbox />)
-    .add("with props", () => <Checkbox label="Hey"/>);
