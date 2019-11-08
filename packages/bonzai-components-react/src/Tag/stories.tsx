191869aa3305e33cfe6c883a8f8fd67ca5081073
Sameer Marathe <sameer.marathe@bonzai.ad>
2019-11-08 18:21:18 +0530

    feat(ui): Bonzai component repo
    Updated tag component
    
    For: ENG-58
    Change-Id: I8ecc3d3da363caea64cfe51d3acf33f310c96e7a

--- packages/bonzai-components-react/src/Tag/stories.tsx
+++ packages/bonzai-components-react/src/Tag/stories.tsx
@@ -5,13 +5,37 @@ import { Tag } from ".";
 
 storiesOf("Tags", module)
     .add("Default", () => {
-        const props = {
-            content: "This is a tag",
+        const props1 = {
+            content: "Grey tag",
             onClick: action("onClick")
+        };
+        const props2 = {
+            content: "Green tag",
+            onClick: action("onClick"),
+            kind: "green" as const
+        };
+        const props3 = {
+            content: "Blue tag",
+            onClick: action("onClick"),
+            kind: "blue" as const
+        };
+        const props4 = {
+            content: "Red tag",
+            onClick: action("onClick"),
+            kind: "red" as const
+        };
+        const props5 = {
+            content: "Yellow tag",
+            onClick: action("onClick"),
+            kind: "yellow" as const
         };
         return (
             <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-                <Tag {...props}></Tag>
+                <Tag {...props1}></Tag>
+                <Tag {...props2}></Tag>
+                <Tag {...props3}></Tag>
+                <Tag {...props4}></Tag>
+                <Tag {...props5}></Tag>
             </div>
         );
     })
