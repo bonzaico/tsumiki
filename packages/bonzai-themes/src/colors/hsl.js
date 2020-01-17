f19c0ec9f259a53399ca731babbdd039022a44a9
Debjit Biswas <debjit.biswas@bonzai.co>
2020-01-17 14:43:06 +0530

    fix(styles): Fix disabled button styles
    Change-Id: I4e7c1f175785cfe68c93da8b47d1d012946b2303

--- packages/bonzai-themes/src/colors/hsl.js
+++ packages/bonzai-themes/src/colors/hsl.js
@@ -1,9 +1,26 @@
-var hsla = function (r, g, b, opacity) {
-  return `hsla(${r}, ${g}%, ${b}%, ${opacity})`;
-}
+var hsla = function(r, g, b, opacity) {
+    return `hsla(${r}, ${g}%, ${b}%, ${opacity})`;
+};
 
-var hsl = function (r, g, b) {
+var hsl = function(r, g, b) {
     return hsla(r, g, b, 1);
-}
+};
 
-module.exports = { hsla: hsla, hsl: hsl };
+const parse = function(hsl) {
+    const [h, s, l, a] = hsl
+        .replace(/^hsla\(/, "")
+        .replace(/\)$/, "")
+        .split(",")
+        .map(s => s.trim())
+        .map(s => s.replace(/\%/, ""));
+
+    return { h, s, l, a };
+};
+
+const opacity = function(a, hsl) {
+    var { h, s, l } = parse(hsl);
+
+    return hsla(h, s, l, a);
+};
+
+module.exports = { hsla: hsla, hsl: hsl, opacity };
